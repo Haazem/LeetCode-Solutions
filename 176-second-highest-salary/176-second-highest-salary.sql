efe7553a5f7d34af4d76d0ad4d7ff35c210dab82
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-07-13 14:20:34 +0200

    Time: 685 ms (66.40%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,5 +1,16 @@
-
-SELECT MAX(salary) as SecondHighestSalary 
+WITH CTE
+AS
+(
+SELECT salary ,
+	   DENSE_RANK() OVER(ORDER BY salary DESC ) as d_r
 FROM Employee
-WHERE salary < (SELECT MAX(salary) FROM Employee)
+)
+SELECT CASE WHEN COUNT(*) = 0 THEN NULL 
+		    ELSE (SELECT DISTINCT salary   
+				 FROM CTE 
+				 WHERE d_r = 2 
+				) END AS SecondHighestSalary
+
+FROM CTE
+WHERE d_r = 2;
 
