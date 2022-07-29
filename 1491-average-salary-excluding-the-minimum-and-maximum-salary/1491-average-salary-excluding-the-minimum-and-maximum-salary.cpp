51b103974f2ef005e203d095ade30ea339baa85d
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-07-29 23:59:32 +0200

    Time: 5 ms (21.49%), Space: 7.1 MB (40.43%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
-        int min = *min_element(salary.begin(), salary.end());
-	int max = *max_element(salary.begin() , salary.end());
+        int mn = 1000009;
+	int mx = -1;
 	int n = salary.size();
 	double sm = 0 ;
 	for (int i = 0 ; i < n ; i ++ )
 	{
-	 if(salary[i] == min || salary[i] == max)
-		 continue;
-	 else
+		 mn = min(salary[i] , mn);
+		 mx = max(salary[i] , mx);
 		 sm += salary[i];
 	}
 
 	cout << std::fixed;
 	cout << std::setprecision(5);
-	return sm / (n - 2);
+	return (sm - (mn + mx)) / (n - 2);
     }
 };
