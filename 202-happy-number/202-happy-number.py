7f9102f4aedf640d471cfb05e329c05b7e1a2d3c
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-08-02 00:54:41 +0200

    Time: 57 ms (44.06%), Space: 13.9 MB (14.36%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,16 +1,16 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-                s=0
-                res=[]
-                while True:
-                    if s in res:
-                        return False
-                    res.append(n)
-                    s=0
-                    for i in str(n):
-                        s+=(int(i)**2)
-                    if s==1:
-                        return True
-                    n=s
-                return True
+            s = 0
+            res = [] 
+            while True:
+                if s in res :
+                    return False
+                res.append(n)
+                s = 0  
+                for j in str(n):
+                    s += (int(j) ** 2 )
+                if s == 1 :
+                    return True 
+                n = s 
+
         
