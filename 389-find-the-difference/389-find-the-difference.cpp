eaefaf94af5618ed9ea88e4c15a55973a38b8142
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-08-05 11:42:38 +0200

    Time: 14 ms (5.27%), Space: 7 MB (9.38%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        	int n = s.size();
+	int m = t.size();
+	map<char , int> mp , mp2 ;
+	char c ;
+	for (int i = 0 ; i < n ; i ++ )
+	{
+		mp[s[i]] ++;
+	}
+
+	for (int i = 0 ; i < m ; i ++ )
+	{
+		mp2[t[i]] ++;
+
+	}
+
+	for (auto i : t)
+	{
+		if (mp2[i] == 0 || mp2[i] > mp[i])
+		{
+			c = i ;
+		}
+	}
+
+	return c;
+    }
+};
