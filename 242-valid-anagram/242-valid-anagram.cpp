921ab3bac2d241ce1fd70388d87735e0e1e92cfa
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-08-08 23:13:04 +0200

    Time: 7 ms (87.59%), Space: 7.1 MB (99.41%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin() , s.end());
-	sort(t.begin() , t.end());
-
-	return s == t ;
+        	if(s.size() != t.size())return false ;
+	
+	vector<int> x (26 , 0);
+	vector<int> y (26 , 0);
+	
+	for (int i = 0 ; i < s.size() ; i ++ )
+	{
+		x[s[i] - 'a'] ++;
+		y[t[i] - 'a'] ++;
+	}
+	
+	return x == y;
     }
 };
