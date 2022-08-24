29685a2368832c00042f2d98ee84d6850d93440a
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-08-24 22:27:11 +0200

    Time: 76 ms (79.96%), Space: 72.6 MB (5.32%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
-        
-	vector<bool> res;
-	//int n = nums.size();
-	int m = l.size();
-	for(int i = 0 ; i < m ; i ++ )
+    
+bool check(vector<int> nums , int& l , int& r){
+
+	sort(nums.begin()+l , nums.begin()+r+1); // sort sub-array
+	int diff = nums[l+1]-nums[l];
+	for(int i = l+1 ; i<= r; i++)
 	{
-		if(r[i] - l[i] + 1 < 2)
-			continue;
-
-		vector<int> temp;
-		for(int j = l[i] ; j <= r[i] ; j++)
-		{
-			temp.push_back(nums[j]);
-		}
-
-		sort(temp.begin() , temp.end());
-		int diff = temp[1]-temp[0];
-		bool ok = true;
-		for(int j = 0 ; j < (int) temp.size()-1 ; j ++ )
-		{
-			if( temp[j+1] - temp[j] != diff)
-			{
-				ok = false ;
-				break;
-			}
-
-		}
-		if(ok)
-			res.push_back(true);
-		else
-			res.push_back(false);
+		if(nums[i] - nums[i-1] != diff)
+			return false ;
 	}
-	return res;
-
+	return true;
+}
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        vector<bool> ans;
+	for(int i = 0 ; i <(int) l.size() ; i ++ ){
+		if(check(nums , l[i] , r[i])){
+			ans.push_back(true);
+		}
+		else {
+			ans.push_back(false);
+		}
+	}
+	return ans;
     }
 };
