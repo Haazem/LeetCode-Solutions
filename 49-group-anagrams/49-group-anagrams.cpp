a110bdf820889dbbc9a852828720f59e6d4bc544
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-08-25 10:10:25 +0200

    Time: 110 ms (13.78%), Space: 20.9 MB (34.53%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        //sort(strs.begin() , strs.end());
+	int n = strs.size();
+	vector<vector<string>> res ;
+	map<string , vector<string>> mp ;
+	for(int i = 0 ; i < n ; i ++ )
+	{
+		string temp = strs[i];
+		sort(temp.begin() , temp.end());
+		mp[temp].push_back(strs[i]);
+	}
+
+	for(auto i : mp)
+	{
+		res.push_back(i.second);
+	}
+
+	return res;
+    }
+};
