10b8e42a28d87b7ffa41fbb930a3ead45568c110
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-09-02 15:00:58 +0200

    Time: 9 ms (33.65%), Space: 9.5 MB (78.52%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-    
-int do_BinSearch(vector<int>& nums ,int target){
-	vector<int> temp;
-	temp = nums;
-	sort(temp.begin() , temp.end());
-
-	int l = 0 , r = temp.size()-1;
-	int mid ;
-	while(l <= r){
-		mid = (l + r) / 2 ;
-		if(temp[mid] > target){
-			r = mid - 1;
-		}
-		else if (temp[mid] < target){
-			l = mid + 1;
-		}
-		else
-			return mid;
-	}
-	return -1;
-}
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        	int n = matrix.size();
-	int ans = -1;
-
-	for(int i = 0 ; i < n ; i ++ )
-	{
-		ans = do_BinSearch(matrix[i] , target);
-		if (ans != -1)
+        int n = matrix.size();
+	int m = matrix[0].size();
+	int k = n*m-1;
+	int mid;
+	int l = 0,r=k;
+	while(l<=r){
+		mid = l + (r - l) / 2 ;
+		int row = mid/m;
+		int col = mid%m;
+		if(matrix[row][col] == target){
 			return true;
+		}else if (matrix[row][col] > target){
+			r = mid - 1;
+		}else {
+			l = mid+1;
+		}
+
 	}
 	return false;
-
     }
 };
