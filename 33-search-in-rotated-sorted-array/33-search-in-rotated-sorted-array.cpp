2b2d7fbe9a8262a12a75475929e7f8defe85c50e
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-09-02 12:02:11 +0200

    Time: 3 ms (88.92%), Space: 11.1 MB (29.36%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,59 +1,31 @@
 class Solution {
 public:
-    
-    int do_BinSearch(vector<int>& nums ,int target){
-	vector<int> temp;
-	temp = nums;
-	sort(temp.begin() , temp.end());
-
-	int l = 0 , r = temp.size()-1;
-	int mid ;
-	while(l <= r){
-		mid = (l + r) / 2 ;
-		if(temp[mid] > target){
-			r = mid - 1;
-		}
-		else if (temp[mid] < target){
-			l = mid + 1;
-		}
-		else
-			return mid;
-	}
-	return -1;
-}
     int search(vector<int>& nums, int target) {
-        
-	int n = nums.size();
-	int k1 = 0 , k2 = 0 ;
-	map<int , int> mp;
-	bool flage = false;
-	for (int i = 0 ; i < n-1 ; i ++ )
-	{
-		if(nums[i] > nums[i+1])
-		{
-			mp[nums[i]] = 1;
-			k1 = i+1;
-			flage = true;
-			break;
-		}
-		else
-			mp[nums[i]] = 1;
-	}
+        	int s = 0 , e = nums.size()-1;
+	 while(s<=e){
+	        int mid=(s+e)/2;
 
+	        if(nums[mid]==target){
+	            return mid;
+	        }
 
-	k2 = n - k1;
+	        if(nums[s]<=nums[mid]){
+	            if(target >= nums[s] && target<=nums[mid]){
+	            	e=mid-1;
+	            }else{
 
-	int r = do_BinSearch(nums, target);
-	if (r == -1)return -1;
-	if(!flage) return r ;
-	int ans = 0 ;
-	if(mp[target] == 1){
-		ans = r - k2;
-	}
-	else {
-		ans = r + k1;
-	}
+	                s=mid+1;
+	            }
+	        }
+	        else{
+	            if(target<=nums[e] && target>=nums[mid]){
+	                s=mid+1;
+	            }else{
+	                e=mid-1;
+	            }
+	        }
+	    }
+	    return -1;
 
-	return ans ;
     }
 };
