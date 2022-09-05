a5a6c53f81c03d2c70c2fcf1fab6533c2939b10a
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-09-05 23:57:17 +0200

    Time: 78 ms (14.94%), Space: 19.2 MB (21.36%) - LeetHub

---
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        	sort(firstList.begin() , firstList.end());
+	sort(secondList.begin() , secondList.end());
+	int n = firstList.size();
+	int m = secondList.size();
+
+	vector<vector<int>> ans ;
+
+	int p1 = 0 ,p2 = 0 ;
+	while (p1 < n && p2 < m ){
+
+		vector<int> temp ;
+		//e2 >= s1 && e1 >= s2
+		if(secondList[p2][1] >= firstList[p1][0] && firstList[p1][1] >= secondList[p2][0]){
+			int mx = max(firstList[p1][0] , secondList[p2][0]);
+			int mn = min(firstList[p1][1] , secondList[p2][1]);
+			temp = {mx , mn};
+			ans.push_back(temp);
+		}
+		if (firstList[p1][1] < secondList[p2][1]){
+				p1++;
+			} else {
+				p2++;
+			}
+	}
+
+	while(p1 > n ){
+		ans.push_back({firstList[p1][0] , firstList[p1][1]});
+		p1++;
+	}
+	while(p2 > m ){
+		ans.push_back({secondList[p2][0] , secondList[p2][1]});
+		p2++;
+	}
+
+	return ans;
+    }
+};
