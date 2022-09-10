76e6f4e4739a39c010d034add51e6f47d158c6ee
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-09-11 00:13:22 +0200

    Time: 26 ms (88.11%), Space: 14.7 MB (25.22%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,57 +1,45 @@
 class Solution {
 public:
-    
-    
-typedef vector<vector<int>> GRAPH;
+
+    typedef vector<vector<int>> GRAPH;
 
 void add_undirected_edge(GRAPH &graph, int from, int to) {
 	graph[from].push_back(to);
 
 }
 
-void BFS( vector<vector<int>>& graph , int start , vector<int>& vis){
+    
+    void dfs(vector<int>& vis , GRAPH &graph , int start){
 
-	queue<int> q ;
-	q.push(start);
-	vis[start] = 1;
-
-	while(!q.empty()){
-	int cur = q.front();
-	q.pop();
-
-	for (int neighbor : graph[cur] ){
-		if (vis[neighbor] == 0){
-			vis[neighbor] = 1;
-			q.push(neighbor);
-		 }
-	   }
-
+	for (auto i : graph[start]){
+		if (!vis[i]){
+			vis[i] = 1;
+			dfs(vis , graph , i);
+		}
 	}
 }
 
-
-    
     
     int findCircleNum(vector<vector<int>>& isConnected) {
         	int cnt = 0 ;
 	int n = isConnected.size();
-	vector<int> vis(n , 0);
 	GRAPH graph(n);
-
 	for(int i = 0 ; i < n ; i ++){
-		for (int j = 0 ; j < n ; j ++ ){
-			if (isConnected[i][j] == 1 && i != j)
-			add_undirected_edge(graph, i, j);
-		}
-	 }
-
-	for (int i = 0 ; i < n ; i ++ ){
-		if (vis[i] == 0 ){
-			cnt ++;
-			BFS(graph, i, vis);
+		for(int j = 0 ; j < n ; j ++){
+			if(isConnected[i][j] == 1){
+				add_undirected_edge(graph, i, j);
+			}
 		}
 	}
 
+	vector<int> vis(n , 0);
+	for(int i = 0 ; i < n ; i ++){
+		if (vis[i] == 0){
+			dfs(vis , graph , i);
+			cnt ++;
+		}
+	}
 	return cnt;
+	
     }
 };
