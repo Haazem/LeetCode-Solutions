90277a5e3524cf556b7d7364440e65b1fe19ef86
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-09-13 13:57:04 +0200

    Time: 58 ms (40.73%), Space: 14.1 MB (35.98%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,14 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-            n = len(nums)
-            toatal_size = pow(2 , n)
-            ans = []
-            for i in range(toatal_size):
-                temp = []
-                for j in range(n):
-                    if i & (1 << j) :
-                        temp.append(nums[j])
-                ans.append(temp)
-            return ans
+            res = []
+            subset = []
+            def dfs(index):
+                if index >= len(nums):
+                    res.append(subset.copy())
+                    return
+
+                subset.append(nums[index])
+                dfs(index + 1)
+                subset.pop()
+                dfs(index + 1)
+
+            dfs(0)
+            return res
 
         
