14bbec48cfd20616a25c9a67cae9a7fba922e3bd
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-09-13 14:03:31 +0200

    Time: 0 ms (100.00%), Space: 7.2 MB (57.43%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        	
+	vector<vector<int>> res ; 
+	
+	int n = nums.size();
+	unsigned int total_size = pow(2 , n);
+	
+	for (int i = 0 ; i < total_size ; i ++){
+		vector<int> temp;
+			
+		for (int j = 0 ; j < n ; j ++){
+			if(i & (1 << j)){
+				temp.push_back(nums[j]);
+			}
+		}
+		res.push_back(temp);
+	}
+	
+	return res;
+    }
+};
