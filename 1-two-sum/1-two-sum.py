125c4dc7c4c1a2eaeef839937ab0ea2a4acc41e1
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-09-19 23:03:18 +0200

    Time: 152 ms (37.60%), Space: 16.3 MB (8.81%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,14 +1,29 @@
 class Solution:
+    from collections import defaultdict
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-                d = {}
-                for idx , num in enumerate(nums):
-                    need = target - num
-                    if need in d :
-                        return [d[need] , idx ]
-                    else :
-                        d[num] = idx 
+            d = defaultdict(list)
+            for i in range(len(nums)):
+                d[nums[i]].append(i)
+            nums.sort()
+            i = 0
+            j = len(nums)-1
+            ans = []
+            while i < j :
+                sm = nums[i] + nums[j]
+                if sm > target:
+                    j -=1
+                elif sm < target:
+                    i+=1
+                else:
+                    ans.append(nums[i])
+                    ans.append(nums[j])
+                    break
 
-                return []
-
+            x = d[ans[0]][0]
+            y = d[ans[1]][0]
+            if x == y :
+                y = d[ans[1]][1]
+            return x , y
 
         
