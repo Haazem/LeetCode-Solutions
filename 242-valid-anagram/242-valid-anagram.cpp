b2d53ce78c20b54126e9829ea49813599a6c8f14
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-09-20 21:29:04 +0200

    Time: 6 ms (91.11%), Space: 7.4 MB (49.47%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        	
-	sort(s.begin() , s.end());
-	sort(t.begin() , t.end());
-	return s == t ;
+        
+	int count1[30] = {0};
+	int count2[30] = {0};
+
+	for(int i = 0 ; i < (int)s.size() ; i ++){
+			count1[s[i] - 'a'] ++;
+	}
+	for(int i = 0 ; i < (int)t.size() ; i ++){
+		   count2[t[i] - 'a'] ++;
+	}
+
+	for (int i = 0 ; i < 30 ; i ++){
+		if (count1[i] != count2[i])
+			return false;
+	}
+
+	return true;
+
     }
 };
