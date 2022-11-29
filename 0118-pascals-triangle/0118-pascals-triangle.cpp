be590fd18c101509e68dd409d6c13784fb4687ae
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-11-29 17:58:46 +0200

    Time: 4 ms (24.27%), Space: 6.4 MB (90.37%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,35 +2,28 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         
-        	vector<vector<int>> ans;
-	if(numRows == 1)
-	{
-		ans.push_back({1});
+        
+        
+	vector<vector<int>> ans;
+	ans.push_back({1});
 
-	}else if (numRows == 2){
-		ans.push_back({1});
-		ans.push_back({1 , 1});
-
-	}else {
-
-		ans.push_back({1});
-		ans.push_back({1 , 1});
-
-		for(int i = 2 ; i < numRows ; i ++){
-			vector<int> v;
-			v.push_back(1);
-			for(int j = 0 ; j < (int)ans[i-1].size() - 1; j ++ ){
-
-				v.push_back(ans[i-1][j] + ans[i-1][j + 1]);
-
-			}
-			v.push_back(1);
-			ans.push_back(v);
+	for(int i = 0 ; i < numRows - 1; i ++ ){
+		vector<int> temp;
+		temp.push_back(0);
+		for(auto k : ans[i]){
+			temp.push_back(k);
 		}
+		temp.push_back(0);
+		vector<int> new_row;
+		for(int j = 0 ; j < (int)ans[i].size() + 1 ; j ++){
+			new_row.push_back(temp[j] + temp[j + 1]);
+		}
+
+		ans.push_back(new_row);
 	}
 
 	return ans;
-
+        
         
     }
 };
