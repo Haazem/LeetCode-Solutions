51451bbfc68763c9bef7b3ced41e6026950a1084
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-12-04 09:09:03 +0200

    Time: 11 ms (65.70%), Space: 9.1 MB (14.01%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-   
-    int findNextGreater(vector<int>& nums2 , int index){
-
-	for(int i = index + 1 ; i < (int)nums2.size() ; i ++){
-		if(nums2[i] > nums2[index]){
-			return nums2[i];
-		}
-	}
-        return -1;
-    }
-
-
-
-    
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
         
-   vector<int> ans;
+	int n = (int)nums1.size();
 	map<int , int>mp;
-	for(int i = 0 ; i < (int) nums2.size() ; i ++){
-		mp[nums2[i]] = findNextGreater(nums2, i);
+	for(int i = 0 ; i < n ; i ++ ){
+		mp[nums1[i]] = i; // val , idx
 	}
 
-	
-	for(int i = 0 ; i < (int)nums1.size() ; i ++){
-		ans.push_back(mp[nums1[i]]);
-	}
+	vector<int>ans(n , -1);
+	stack<int> s;
 
+	for(int i = 0 ; i < (int)nums2.size(); i ++){
+		int cur_val = nums2[i];
+
+		while(!s.empty() && cur_val > s.top()){
+			int top_val = s.top();
+			s.pop();
+			int idx = mp[top_val];
+			ans[idx] = cur_val;
+		}
+
+		if(mp.find(cur_val) != mp.end()){
+			s.push(cur_val);
+		}
+	}
 
 	return ans;
-
         
         
     }
