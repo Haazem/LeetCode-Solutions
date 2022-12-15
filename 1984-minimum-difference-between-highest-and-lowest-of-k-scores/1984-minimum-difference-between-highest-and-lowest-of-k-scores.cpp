8d11ac0ac9eb784d55e02d377afa319121027eff
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-12-15 12:52:42 +0200

    Time: 15 ms (95.65%), Space: 13.8 MB (26.02%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -2,39 +2,19 @@ class Solution {
 public:
     int minimumDifference(vector<int>& nums, int k) {
         
-        
-	int n = (int)nums.size();
-	int ans = 100000000;
-	sort(nums.begin(), nums.end());
+        int ans = 100000009;
+        sort(nums.begin(),nums.end());
+        int n = (int)nums.size();
+        int l = 0 , r = k-1;
 
-	for (int i = 0 ; i < n - k + 1 ; i ++){
-		vector<int> temp;
-		temp.push_back(nums[i]);
-		int k2 = k;
-		k2--;
-		for(int j = i+1; j < n ; j ++ ){
-			if(k2 == 0) break;
-			else {
-				temp.push_back(nums[j]);
-				k2 --;
-			}
-		}
-		
-		/*
-		for(auto e : temp)
-		{
-			cout << e << " ";
-		}cout << endl;
-		 */
-		
-		int max_ele = *max_element(temp.begin(),temp.end());
-		int min_ele = *min_element(temp.begin(), temp.end());
+        while(r < n){
+            ans = min(ans, nums[r] - nums[l]);
+            l++;
+            r++;
+        }
 
-		ans = min(ans, max_ele - min_ele);
-	}
+        return ans;
 
-
-return ans;
         
         
     }
