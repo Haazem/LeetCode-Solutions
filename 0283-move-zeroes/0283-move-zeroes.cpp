c616631f73c1587d0b8bb37e650e81c608abb03a
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-12-28 12:35:37 +0200

    Time: 33 ms (69.18%), Space: 19.3 MB (61.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         
-        vector<int> temp;
-	int cnt = 0;
-	for(int i = 0 ; i < (int)nums.size(); i ++){
+        	int l =0 ;
 
-		if(nums[i] != 0)
-			temp.push_back(nums[i]);
-		else
-			cnt ++;
+	for(int i = 0 ; i < (int)nums.size() ; i++){
+
+		if(nums[i] != 0){
+			nums[l ++] = nums[i];
+		}
 	}
 
-	for(int i = 0 ; i < cnt ; i ++){
-		temp.push_back(0);
+	for(int i = l ; i < (int)nums.size(); i ++){
+		nums[i] = 0;
 	}
-
-
-	nums = temp;
+        
         
         
     }
